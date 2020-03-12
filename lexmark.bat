fe9ec058c1e9099900504e596a759e1a66942f6b
pravalli0630 <45756719+pravalli0630@users.noreply.github.com>
2020-03-12 12:06:33 +0000

    Update lexmark.bat

--- lexmark.bat
+++ lexmark.bat
@@ -1,1 +1,1 @@
-call C:\\Python38\\Scripts\\robot.exe C:\\Users\\Administrator\\Desktop\\jenkins\\workspace\\demo\\lexmark.robot
+call C:\\Python38\\Scripts\\robot.exe C:\\jenkins\\workspace\\demo\\lexmark.robot
