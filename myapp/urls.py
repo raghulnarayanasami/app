ae38a01658ce481cd61ee69f3197002a53d08452
pravalli0630 <45756719+pravalli0630@users.noreply.github.com>
2020-03-12 10:08:08 +0000

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
    path('', views.user_login, name='index'),
    path('storage/', views.storagedata, name='storage'),
    path('s3bucket/', views.s3bucket, name='s3bucket'),
+   path('selenium/', views.selenium, name='selenium'),
    path('logout/', views.user_logout, name='logout'),
 
 ]
